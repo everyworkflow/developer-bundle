de56674849697cb66132f45ccfcbe0df3f86f001
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:07:44 +0000

    Added ?ExtensionInterface

--- EveryWorkflowDeveloperBundle.php
+++ EveryWorkflowDeveloperBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\DeveloperBundle;
 
 use EveryWorkflow\DeveloperBundle\DependencyInjection\DeveloperExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowDeveloperBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new DeveloperExtension();
     }
